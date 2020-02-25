adb8732504c1036c72539efa05183121c770f712
Anand Thamildurai <athamildurai@appliedsystems.com>
2020-02-25 14:16:18 -0500

    Add logging

--- Sustainsys.Saml2/WebSSO/LogOutCommand.cs
+++ Sustainsys.Saml2/WebSSO/LogOutCommand.cs
@@ -200,7 +200,7 @@ namespace Sustainsys.Saml2.WebSso
                 var logoutRequest = idp.CreateLogoutRequest(request.User);
 
                 commandResult = Saml2Binding.Get(idp.SingleLogoutServiceBinding)
-                    .Bind(logoutRequest);
+                    .Bind(logoutRequest, options.SPOptions.Logger);
 
                 commandResult.RelayState = logoutRequest.RelayState;
                 commandResult.RequestState = new StoredRequestState(
@@ -287,7 +287,7 @@ namespace Sustainsys.Saml2.WebSso
             options.SPOptions.Logger.WriteInformation("Got a logout request " + request.Id
                 + ", responding with logout response " + response.Id);
 
-            var result = Saml2Binding.Get(idp.SingleLogoutServiceBinding).Bind(response);
+            var result = Saml2Binding.Get(idp.SingleLogoutServiceBinding).Bind(response, options.SPOptions.Logger);
             result.TerminateLocalSession = true;
             return result;
         }
